bd1af87a80a8eb6326a2944cc39053b772848b06
MahmudAlburozi12 <mahmudalburozi10@gmail.com>
2023-12-25 21:28:28 +0700

    F05 Edit Merek

---
+++ admin/merek_edit.php
@@ -0,0 +1,25 @@
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3">
+    <h6><a href="?page=laptop_data">Kembali</a></h6>
+</div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Edit Merek</h1>
+</div>
+
+<div class="row">
+    <div class="col">
+        <form action="" method="POST" id="formLaptop">
+            <div class="form-group">
+                <label for="kode">Kode</label>
+                <input type="text" class="form-control" id="kode" name="kode" readonly />
+            </div>
+            <div class="form-group">
+                <label for="nama_kategori">Nama Kategori</label>
+                <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" />
+            </div>
+            <div class="form-group">
+                <button class="btn btn-danger" type="reset">Reset</button>
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
+</div>
