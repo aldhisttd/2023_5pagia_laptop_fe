beade439e6cfaf05102265f4f7cfeab714b31925
agungsyhptra <agungsyhptr05@gmail.com>
2023-12-27 00:07:03 +0700

    F09-Tabel-Data-Delete-Laptop

---
+++ admin/laptop_data.php
@@ -0,0 +1,17 @@
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Kode</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Kategori</th>
+      <th scope="col">Merek</th>
+      <th scope="col">Gambar</th>
+      <th scope="col">Harga</th>
+      <th scope="col">Deskripsi</th>
+      <th scope="col">Aksi</th>
+    </tr>
+  </thead>
+  <tbody id="datalaptop">
+  </tbody>
+</table>
---
+++ admin/script/laptop_data.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+    // Fungsi untuk memuat data menu
+    function loadLaptopData() {
+        $.ajax({
+            type: "GET",
+            url: host + "laptop_read.php",
+            dataType: "json",
+            success: function (response) {
+                var datalaptop = response.body.data;
+
+                // Hapus data yang ada
+                $("#datalaptop").empty();
+
+                // Muat ulang data menu setelah penghapusan berhasil
+                for (var i = 0; i < datalaptop.length; i++) {
+                    $("#datalaptop").append(
+                        `
+                        <tr>
+                            <td>` +
+                        (i + 1) +
+                        `</td>
+                            <td>` +
+                        datalaptop[i].kode +
+                        `</td>
+                            <td>
+                            <img src="../assets/img/` +
+                        datalaptop[i].gambar +
+                        `" width="100" />
+                            </td>
+                            <td>` +
+                        datalaptop[i].nama +
+                        `</td>
+                            <td>` +
+                        datalaptop[i].kode_kategori +
+                        `</td>
+                            <td>` +
+                        datalaptop[i].kode_merek +
+                        `</td>
+                            <td>` +
+                        datalaptop[i].deskripsi +
+                        `</td>
+                            <td>` +
+                        datalaptop[i].harga +
+                        `</td>
+                            <td>
+                            <button class="btn btn-danger delete-btn" data-kode="` +
+                        datalaptop[i].kode +
+                        `">Hapus</button>
+                            <button class="btn btn-primary edit-btn">
+                                <a class="text-light" href="../obat_edit/menu_edit.php?kode=` +
+                        datalaptop[i].kode +
+                        `">Edit</a>
+                                </button>
+                            </td>
+                        </tr>
+                        `
+                    );
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error("AJAX Error: " + status, error);
+            }
+        });
+    }
+
+    // Fungsi untuk menangani klik tombol hapus
+    function handleDeleteButtonClick() {
+        $(document).on("click", ".delete-btn", function () {
+            var kode = $(this).data("kode");
+
+            if (confirm("Yakin ingin hapus data dengan kode=" + kode + "?")) {
+                $.ajax({
+                    type: "POST",
+                    url: host + "laptop_delete.php",
+                    data: { kode: kode },
+                    dataType: "json",
+                    success: function (response) {
+                        alert(response.msg);
+                    },
+                    error: function (xhr, status, error) {
+                        console.error("AJAX Error: " + status, error);
+                    }
+                });
+            }
+        });
+    }
+
+    // Panggil fungsi loadLaptopData saat memuat halaman
+    loadLaptopData();
+
+    // Panggil fungsi handleDeleteButtonClick untuk menangani klik tombol hapus
+    handleDeleteButtonClick();
+});
