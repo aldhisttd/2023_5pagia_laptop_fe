7c938a87ccbedfe9781352e521328a2f94e3850a
Yutagaru <muhammadmami12g@gmail.com>
2023-12-30 13:39:01 +0700

    F07-input-laptop

--- admin/laptop_create.php
+++ admin/laptop_create.php
@@ -1,13 +1,13 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Input Produk</h1>
+    <h1 class="h2">Input Laptop</h1>
 </div>
 
 <div class="row">
     <div class="col">
         <form id="insert-laptop">
          <div class="form-group">
-                <label for="nama">Kode</label>
-                <input type="text" class="form-control" name="nama" id="nama">
+                <label for="kode">Kode</label>
+                <input type="text" class="form-control" name="kode" id="kode">
             </div>  
             <div class="form-group">
                 <label for="nama">Nama Laptop</label>
@@ -15,17 +15,11 @@
             </div>                    
             <div class="form-group">
                 <label for="kategori">Kategori</label>
-                <select class="form-control" id="kategori" name="kode_kategori">
-                <option>Vivobook</option>
-                <option>Thinkpad</option>
-                </select>
+                <select class="form-control" id="kategori" name="kode_kategori"></select>
             </div>
             <div class="form-group">
                 <label for="merek">Merek</label>
-                <select class="form-control" id="merek" name="kode_merek">
-                <option>Asus</option>
-                <option>Lenovo</option>
-                </select>
+                <select class="form-control" id="merek" name="kode_merek"></select>
             </div>
             <div class="form-group">
                 <label for="gambar">Gambar</label>
--- admin/script/laptop_create.js
+++ admin/script/laptop_create.js
@@ -1,24 +1,69 @@
-$(document).ready(function () {
-    $('#insert-laptop').submit(function (event) {
-        event.preventDefault();
+var host = 'http://localhost/coba/2023_5pagia_laptop_be/dummy.php';
+$.ajax({
+  type: 'GET',
+  url: host + 'kategori_read_one.php',
+  cache: false,
+  contentType: false,
+  processData: false,
+  dataType: 'json',
+  success: (result) => {
+    // Clear existing options in the dropdown
+    $('#kode_kategori').empty();
 
-        var formData = new FormData(this);
+    // Add an empty/default option
+    $('#kode_kategori').append('<option value="">Pilih Kategori</option>');
 
-        $.ajax({
-            type: 'POST',
-            url: "http://localhost/2023_5pagia_laptop_fe/url_api_spec/laptop_input.php",
-            data: formData,
-            dataType: 'json',
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                console.log(response);
-                alert('Data berhasil ditambahkan');
-            },
-            error: function (error) {
-                console.error('Error:', error.responseText);
-                alert('Terjadi kesalahan. Silakan cek konsol untuk informasi lebih lanjut.');
-            },
-        });
+    // Loop through the data and append options to the dropdown
+    result.data.forEach((item) => {
+      $('#kode_kategori').append(`
+                <option value="${item.kode}">${item.nama}</option>
+            `);
     });
+  },
+});
+
+$.ajax({
+  type: 'GET',
+  url: host + 'merek_read_one.php',
+  cache: false,
+  contentType: false,
+  processData: false,
+  dataType: 'json',
+  success: (result) => {
+    // Clear existing options in the dropdown
+    $('#kode_merek').empty();
+
+    // Add an empty/default option
+    $('#kode_merek').append('<option value="">Pilih Merek</option>');
+
+    // Loop through the data and append options to the dropdown
+    result.data.forEach((item) => {
+      $('#kode_merek').append(`
+                  <option value="${item.kode}">${item.nama}</option>
+              `);
+    });
+  },
+});
+
+$('#insert-laptop').submit(function (e) {
+  e.preventDefault();
+  var formData = new FormData(this);
+  console.log(formData);
+  $.ajax({
+    type: 'POST',
+    url: host,
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    dataType: 'json',
+    success: function (response) {
+      console.log(response);
+      alert('Data berhasil ditambahkan');
+  },
+  error: function (error) {
+      console.error('Error:', error.responseText);
+      alert('Terjadi kesalahan. Silakan cek konsol untuk informasi lebih lanjut.');
+  },
+});
 });
