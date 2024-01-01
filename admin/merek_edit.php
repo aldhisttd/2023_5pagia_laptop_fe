dece77e3efd81a478569b10a6a826cf70e37ab5e
MahmudAlburozi12 <mahmudalburozi10@gmail.com>
2024-01-01 13:24:03 +0700

    revisi f05-edit-merek

--- admin/merek_edit.php
+++ admin/merek_edit.php
@@ -1,25 +1,26 @@
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3">
-    <h6><a href="?page=laptop_data">Kembali</a></h6>
-</div>
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Edit Merek</h1>
-</div>
-
-<div class="row">
-    <div class="col">
-        <form action="" method="POST" id="formLaptop">
-            <div class="form-group">
-                <label for="kode">Kode</label>
-                <input type="text" class="form-control" id="kode" name="kode" readonly />
-            </div>
-            <div class="form-group">
-                <label for="nama_kategori">Nama Kategori</label>
-                <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" />
-            </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="reset">Reset</button>
-                <button class="btn btn-primary" type="submit">Submit</button>
-            </div>
-        </form>
+<main role="main" class="col-lg-7 px-md-2">
+    <!-- title -->
+    <div class="row mt-3">
+        <div class="col">
+            <h3>Merek Edit</h3>
+        </div>
     </div>
-</div>
+    <!-- form -->
+    <div class="row mt-2">
+        <div class="col">
+            <form id="merekUpdate" action="" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="kode">Kode</label>
+                    <input type="text" class="form-control" name="kode" id="kode" />
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" name="nama" id="nama" />
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary btn-lg" type="submit">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>  
+</main>
