0287f90462659d7593c4ffc5c78dd74816e63eb9
oktihryn123 <oktiharyani94@gmail.com>
2024-01-03 14:38:33 +0700

    F03-Tabel-Data-Delete-Kategori

---
+++ admin/kategori_data.php
@@ -0,0 +1,24 @@
+<div class="col">
+    <!-- konten -->
+    <div class="row my-4">
+        <div class="col">
+            <h2>Tabel Kategori</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Kode</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody id="kategoriData">
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
