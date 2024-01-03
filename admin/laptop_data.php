0fb1dcb746345c76dfd0923e8179105d08bb3456
agungsyhptra <agungsyhptr05@gmail.com>
2024-01-03 17:49:09 +0700

    revisi

--- admin/laptop_data.php
+++ admin/laptop_data.php
@@ -1,18 +1,25 @@
-<table class="table">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">Data Laptop</h1>
+</div>
+
+
+<table class="table" id="laptopTable">
   <thead>
     <tr>
-      <th scope="col">No</th>
-      <th scope="col">Kode</th>
-      <th scope="col">Nama</th>
-      <th scope="col">Kategori</th>
-      <th scope="col">Merek</th>
-      <th scope="col">Gambar</th>
-      <th scope="col">Harga</th>
-      <th scope="col">Deskripsi</th>
-      <th scope="col">Aksi</th>
+      <th>No</th>
+      <th>Kode</th>
+      <th>Nama</th>
+      <th>Kategori</th>
+      <th>Merek</th>
+      <th>Gambar</th>
+      <th>Harga</th>
+      <th>Deskripsi</th>
+      <th>Aksi</th>
     </tr>
   </thead>
-  <tbody id="datalaptop">
-    
-  </tbody>
+  <tbody id="laptopBody"></tbody>
 </table>
+
+</body>
+
+</html>
--- admin/script/laptop_data.js
+++ admin/script/laptop_data.js
@@ -1,67 +1,44 @@
 $(document).ready(function () {
-    // Fungsi untuk memuat data menu
+    // Fungsi untuk memuat data laptop dari API dan mengisi tabel
     function loadLaptopData() {
         $.ajax({
             type: "GET",
-            url: host + "laptop_read.php",
+            url: host + "laptop_read.php", // Ganti dengan URL API yang sesuai
             dataType: "json",
             success: function (response) {
-                var datalaptop = response.body.data;
+                var laptopList = response.body.data;
 
-                // Hapus data yang ada
-                $("#datalaptop").empty();
+                // Bersihkan baris tabel yang sudah ada
+                $("#laptopBody").empty();
 
-                // Muat ulang data menu setelah penghapusan berhasil
-                for (var i = 0; i < datalaptop.length; i++) {
-                    $("#datalaptop").append(
-                        `
-                        <tr>
-                            <td>` +
-                        (i + 1) +
-                        `</td>
-                            <td>` +
-                        datalaptop[i].kode +
-                        `</td>
-                            <td>
-                            <img src="` +
-                        host +
-                        datalaptop[i].gambar +
-                        `" width="100" />
-                                <td>` +
-                        datalaptop[i].nama +
-                        `</td>
-                                <td>` +
-                        datalaptop[i].kategori +
-                        `</td>
-                            <td>` +
-                        datalaptop[i].merek +
-                        `</td>
-                            <td>` +
-                        datalaptop[i].deskripsi +
-                        `</td>
-                            <td>` +
-                        datalaptop[i].harga +
-                        `</td>
-                            <td>
-                            <button class="btn btn-danger delete-btn" data-kode="` +
-                        datalaptop[i].kode +
-                        `">Hapus</button>
-                            <button class="btn btn-primary edit-btn">
-                                <a class="text-light" href="index.php?page=laptop_edit&kode=` +
-                        datalaptop[i].kode +
-                        `">Edit</a>
-                                        </button>
-                                    </td>
-                                </tr>
-                                `
+                // Isi tabel dengan data laptop
+                for (var i = 0; i < laptopList.length; i++) {
+                    $("#laptopBody").append(
+                        '<tr>' +
+                        '<td>' + (i + 1) + '</td>' +
+                        '<td>' + laptopList[i].kode + '</td>' +
+                        '<td>' + laptopList[i].nama + '</td>' +
+                        '<td>' + laptopList[i].kategori + '</td>' +
+                        '<td>' + laptopList[i].merek + '</td>' +
+                        '<td><img src="' + laptopList[i].gambar + '" width="100" /></td>' +
+                        '<td>' + laptopList[i].harga + '</td>' +
+                        '<td>' + laptopList[i].deskripsi + '</td>' +
+                        '<td>' +
+                        '<button class="btn btn-danger delete-btn" data-kode="' + laptopList[i].kode + '">Hapus</button>' +
+                        '<a class="btn btn-primary" href="index.php?page=laptop_edit&kode=' + laptopList[i].kode + '">Edit</a>' +
+                        '</td>' +
+                        '</tr>'
                     );
                 }
             },
             error: function (xhr, status, error) {
-                console.error("AJAX Error: " + status, error);
+                console.error("Kesalahan AJAX: " + status, error);
             }
         });
     }
+
+    // Memuat data laptop saat halaman dimuat
+    loadLaptopData();
 
     // Fungsi untuk menangani klik tombol hapus
     function handleDeleteButtonClick() {
@@ -69,26 +46,25 @@ $(document).ready(function () {
             var kode = $(this).data("kode");
 
             if (confirm("Yakin ingin hapus data dengan kode=" + kode + "?")) {
+                // Memanggil API untuk menghapus data
                 $.ajax({
                     type: "POST",
-                    url:  host + "laptop_delete.php",
+                    url: host + "laptop_delete.php", // Ganti dengan URL API yang sesuai
                     data: { kode: kode },
                     dataType: "json",
                     success: function (response) {
                         alert(response.msg);
+                        // Memuat ulang data laptop setelah penghapusan berhasil
                         loadLaptopData();
                     },
                     error: function (xhr, status, error) {
-                        console.error("AJAX Error: " + status, error);
+                        console.error("Kesalahan AJAX: " + status, error);
                     }
                 });
             }
         });
     }
 
-    // Panggil fungsi loadLaptopData saat memuat halaman
-    loadLaptopData();
-
-    // Panggil fungsi handleDeleteButtonClick untuk menangani klik tombol hapus
+    // Menambahkan penanganan klik tombol hapus
     handleDeleteButtonClick();
 });
