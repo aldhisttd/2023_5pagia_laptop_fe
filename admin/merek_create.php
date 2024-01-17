e975c1b4c200d56e5661f317fa0929116ec14560
ismiati20 <ismiatiismiati217@gmail.com>
2024-01-17 09:44:34 +0700

    f04 input merek

---
+++ admin/merek_create.php
@@ -0,0 +1,26 @@
+<main role="main" class="col-lg-7 px-md-2">
+    <!-- title -->
+    <div class="row mt-3">
+        <div class="col">
+            <h3>Merek Baru</h3>
+        </div>
+    </div>
+    <!-- form -->
+    <div class="row mt-2">
+        <div class="col">
+            <form id="MerekCreate">
+                <div class="form-group">
+                    <label for="kode">Kode</label>
+                    <input type="text" class="form-control" name="kode" id="kode" />
+                </div>
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" class="form-control" name="nama" id="nama" />
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary btn-lg" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>  
+</main>
